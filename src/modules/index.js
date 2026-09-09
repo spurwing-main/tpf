fbd9d398b8a921aa7a79b78a46c938ca10ded81b
sampilgrim-sw <sam.pilgrim@spurwing.co.uk>
2026-09-09 14:19:49 +0100

    feat: register values media module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,12 @@
 import { initAccordions } from "./accordions.js";
 import { initCardSlider } from "./card-slider.js";
 import { initNav } from "./nav.js";
+import { initValues } from "./values.js";
 
 // Import each module here. Keep the start order explicit.
 export const modules = [
 	{ name: "nav", init: initNav },
 	{ name: "cardSlider", init: initCardSlider },
 	{ name: "accordions", init: initAccordions },
+	{ name: "values", init: initValues },
 ];
--- src/modules/index.test.js
+++ src/modules/index.test.js
@@ -1,9 +1,19 @@
 import { describe, expect, it } from "vitest";
 import { initAccordions } from "./accordions.js";
 import { modules } from "./index.js";
+import { initValues } from "./values.js";
 
 describe("module registry", () => {
 	it("registers the accordion initializer with the site boot sequence", () => {
 		expect(modules).toContainEqual({ name: "accordions", init: initAccordions });
+	});
+
+	it("starts Values after accordions so initial open state is available", () => {
+		const accordionIndex = modules.findIndex(({ init }) => init === initAccordions);
+		const valuesIndex = modules.findIndex(({ init }) => init === initValues);
+
+		expect(accordionIndex).toBeGreaterThanOrEqual(0);
+		expect(valuesIndex).toBe(accordionIndex + 1);
+		expect(modules[valuesIndex]).toEqual({ name: "values", init: initValues });
 	});
 });
